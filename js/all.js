d3e462eb799bcb90f369f5ac780d1f40ebb7a29e
kanboo <wang.kanboo@gmail.com>
2017-10-01 23:53:05 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,21 @@
+function checkField(e) {
+    e.preventdefault();
+
+
+
+
+}
+
+
+var bt_submit = document.querySelector("#submit");
+
+bt_submit.addEventListener("click", checkField);
+
+
+
+$(document).ready(function () {
+    // $('#ProblemRecord').validator();
+
+
+    // $('#ProblemRecord').validator('validate')
+});
