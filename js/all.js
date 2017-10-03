026c15d10ca2973fd29277f58b442c0e47de5083
kanboo <wang.kanboo@gmail.com>
2017-10-03 16:42:09 +0800

    add js-validator

--- js/all.js
+++ js/all.js
@@ -1,21 +1,27 @@
-function checkField(e) {
-    e.preventdefault();
-
-
-
-
-}
-
-
-var bt_submit = document.querySelector("#submit");
-
-bt_submit.addEventListener("click", checkField);
-
-
-
 $(document).ready(function () {
-    // $('#ProblemRecord').validator();
 
 
-    // $('#ProblemRecord').validator('validate')
+    $("#submit").click(function () {
+        if ($('#ProblemRecord').validator('validate').has('.has-error').length) {
+            // 驗證未通過，則不處理
+            console.log('驗證未通過');
+        } else {
+            // 通过后，送出表单
+            alert("已送出表單");
+        }
+    });
+
+    // $('#ProblemRecord').validator().on('submit', function (e) {
+    //     e.preventDefault(); // 防止原始 form 提交表單
+
+    //     if (e.isDefaultPrevented()) {
+    //         // 驗證未通過，則不處理
+    //         console.log('驗證未通過');
+    //         // $('#ProblemRecord').validator('destroy');
+    //         return;
+    //     } else { // 通过后，送出表单
+    //         alert("已送出表單");
+    //     }
+    // });
+
 });
